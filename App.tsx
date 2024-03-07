a142959914863d6f451f3ea2da610ac82e9a5559
ushair <ushairansari@gmail.com>
2024-03-07 22:54:20 +0530

    Completed Shop Grid

--- App.tsx
+++ App.tsx
@@ -9,12 +9,14 @@ import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 import Header from './components/Header';
 import SearchInput from './components/SearchInput';
+import ShopGrid from './components/ShopGrid';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
       <Header />
       <SearchInput />
+      <ShopGrid />
     </SafeAreaView>
   );
 }
---
+++ components/ShopGrid.tsx
@@ -0,0 +1,82 @@
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+
+const shopItems = [
+  {name: 'Electronics', image: require('../assets/gadget.png')},
+  {name: 'Properties', image: require('../assets/house.png')},
+  {name: 'Jobs', image: require('../assets/jobSearch.png')},
+  {name: 'Furniture', image: require('../assets/sofa.png')},
+  {name: 'Cars', image: require('../assets/car.png')},
+  {name: 'Bikes', image: require('../assets/bike.png')},
+  {name: 'Mobiles', image: require('../assets/smartphone.png')},
+  {name: 'Pets', image: require('../assets/pets.png')},
+];
+
+const ShopGrid = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.shopForText}>Shop for</Text>
+        <TouchableOpacity onPress={() => console.log('See all clicked')}>
+          <Text style={styles.showAllText}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.gridContainer}>
+        {shopItems.map((item, index) => (
+          <View style={styles.gridItem} key={index}>
+            <Image source={item.image} style={styles.image} />
+
+            <Text style={styles.shopItemText}>{item.name}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderBottomColor: '#c2c2c2',
+    borderBottomWidth: 0.5,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    marginBottom: 30,
+  },
+  shopForText: {
+    fontSize: 16,
+    color: '#000',
+  },
+  showAllText: {
+    fontSize: 16,
+    color: '#FBCF7B',
+  },
+  shopItemText: {
+    fontSize: 11,
+    color: '#000',
+  },
+  gridContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    gap: 1,
+  },
+  gridItem: {
+    width: '21%',
+    padding: 10,
+    gap: 10,
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+  image: {
+    width: 35,
+    height: 35,
+    marginBottom: 10,
+  },
+});
+
+export default ShopGrid;
